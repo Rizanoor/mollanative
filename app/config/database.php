ce2ae05e31a367cce555410605aeb92fe23c441a
Rizanoor <rizanoorfauzan123@gmail.com>
2025-01-30 15:59:06 +0700

    feat: migrasi assets

---
+++ app/config/database.php
@@ -0,0 +1,24 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+
+try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $pdo = new PDO($dsn, $user, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Koneksi ke database PostgreSQL berhasil!";
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
+?>
--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php echo "hallo"; ?>
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$base_url = $_ENV['BASE_URL'];
+
+// load file home.php
+require __DIR__ . '/app/views/home.php';
+
