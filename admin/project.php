b43339df6e460dc9d052edddfc458c649bf79804
Rizanoor <rizanoorfauzan123@gmail.com>
2025-02-01 21:24:52 +0700

    fix: project alert

--- admin/project.php
+++ admin/project.php
@@ -16,7 +16,6 @@ $message_type = $_SESSION['message_type'] ?? '';
     <?php if (isset($_SESSION['message'])): ?>
         <div class="alert alert-<?= $_SESSION['message_type']; ?> alert-dismissible fade show" role="alert">
             <?= $_SESSION['message']; ?>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         <?php unset($_SESSION['message']); ?>
     <?php endif; ?>
